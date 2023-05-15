0855706023ab1e8fffdd85e2ab62ed3b403c5491
yoshiko <ttkmysk021@gmail.com>
2023-05-15 20:41:48 +0900

    first creat

---
+++ script.js
@@ -0,0 +1,17 @@
+ // 1
+    var variableBoolean = true ;
+    if(variableBoolean = true){
+      console.log(variableBoolean);
+    }
+
+    // 2
+    var num = 100;
+    if(num == 100){
+      console.log(num);
+    }
+    
+    // 3
+    var text = "JavaScript";
+    if( text == "JavaScript"){
+      console.log(text);
+    }
